d5f51a93ab04252b837dfee1653457498a081c39
WoosungChoi <solut2000@gmail.com>
2020-09-05 06:23:13 +0900

    change acme.sh start command

--- ssl_renew.sh
+++ ssl_renew.sh
@@ -4,4 +4,4 @@ COMPOSE="/usr/local/bin/docker-compose --no-ansi"
 
 cd /home/sammy/gnuboard/
 
-$COMPOSE up --force-recreate --no-deps acme.sh && $COMPOSE kill -s SIGHUP nginx
+$COMPOSE up acme.sh && $COMPOSE kill -s SIGHUP nginx
